f4b2ca567ad55c827f637a8725f1e569c40b10f1
LaloSL <127783873+LaloSL@users.noreply.github.com>
2023-10-11 00:25:03 -0300

    subo el eje 5 pero hay que ver donde esta el error

--- src/universidadejemplo/UniversidadEjemplo.java
+++ src/universidadejemplo/UniversidadEjemplo.java
@@ -14,23 +14,58 @@ public class UniversidadEjemplo {
             Class.forName("org.mariadb.jdbc.Driver");
 
             //Establecer la conexión:
-            String URL = "jdbc:mariadb://localhost:3306/tp13_universidad";
+            String URL = "jdbc:mariadb://localhost:3306/tp13-ulp";
             String usuario = "root";
             String password = "";
-            Connection con = DriverManager.getConnection(URL, usuario, password);
-
+            Connection conexion = DriverManager.getConnection(URL, usuario, password);
+            
+            
             //Carga de Tabla empleado 
-            String sql = "INSERT INTO alumno (dni,apellido,nombre,fechaNacimiento,estado)"
-                    + "VALUES (12345671,'Veron','Juan','2001-01-22',1),(12345672,'Milito','Gabriel','2002-02-12',1),(12345673,'Perez','Juan','2001-03-12',1)";
+            //String sql = "INSERT INTO alumno (dni,apellido,nombre,fechaNacimiento,estado)"
+               //     + "VALUES (12345671,'Veron','Juan','2001-01-22',1),(12345672,'Milito','Gabriel','2002-02-12',1),(12345673,'Perez','Juan','2001-03-12',1)";
 
-            PreparedStatement ps = con.prepareStatement(sql);
-            int filas = ps.executeUpdate();
+//            PreparedStatement ps = con.prepareStatement(sql);
+//            int filas = ps.executeUpdate();
+//
+//            if (filas > 0) {
+//
+//                JOptionPane.showMessageDialog(null, "Alumno Agregado");
+//
+//            }
+            
+        
+          
+           
+            //Ejercicio 5 (NO SE CUAL ES EL ERROR, CREO Q ES ALGO DEL CATCH FALTA UN TRY
+//            String sql= “INSERT INTO inscripcion(idInscripto, nota, idAlumno, idMateria)”;
+//            +” VALUES (1,8,1,1)”;
+//            String sql1= “INSERT INTO inscripcion(idInscripto, nota, idAlumno, idMateria)”;
+//            +” VALUES (2,7,1,2)”;
+//            String sql2= “INSERT INTO inscripcion(idInscripto, nota, idAlumno, idMateria)”;
+//            +” VALUES (3,10,2,2)”;
+//            String sql3= “INSERT INTO inscripcion(idInscripto, nota, idAlumno, idMateria)”;
+//            +” VALUES (4,9,2,3)”;
+//            String sql4= “INSERT INTO inscripcion(idInscripto, nota, idAlumno, idMateria)”;
+//            +” VALUES (5,2,3,1)”;
+//            String sql5= “INSERT INTO inscripcion(idInscripto, nota, idAlumno, idMateria)”;
+//            +” VALUES (6,6,3,4)”;
+//
+//            PreparedStatement ps = conexion.prepareStatement(sql);
+//            ps.executeUpdate();
+//
+//            PreparedStatement ps1 = conexion.prepareStatement(sql1);
+//            ps1.executeUpdate();
+//            PreparedStatement ps2 = conexion.prepareStatement(sql2);
+//            ps2.executeUpdate();
+//            PreparedStatement ps3 = conexion.prepareStatement(sql3);
+//            ps3.executeUpdate();
+//            PreparedStatement ps4 = conexion.prepareStatement(sql4);
+//            ps4.executeUpdate();
+//            PreparedStatement ps5 = conexion.prepareStatement(sql5);
+//            ps5.executeUpdate();
 
-            if (filas > 0) {
-
-                JOptionPane.showMessageDialog(null, "Alumno Agregado");
-
-            }
+            
+            
 
         } catch (ClassNotFoundException ex) {
             JOptionPane.showMessageDialog(null, "Error al cargar el Driver");
